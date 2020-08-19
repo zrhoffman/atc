02dd8a062d2f767af288d40ab884808fe61280d5
Zach Hoffman <zach@zrhoffman.net>
2020-08-18 21:24:14 -0600

    Build ATC component RPMs in parallel jobs (no parallel docker-compose build)

--- .github/actions/run-ciab/entrypoint.sh
+++ .github/actions/run-ciab/entrypoint.sh
@@ -16,8 +16,12 @@
 # specific language governing permissions and limitations
 # under the License.
 
-set -ex
+set -ex;
 export COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 # use Docker BuildKit for better image building performance
+(
+cd dist;
+mv -- */*.rpm .;
+);
 
 docker-compose --version;
 cd infrastructure/cdn-in-a-box;
@@ -27,7 +31,7 @@ docker images;
 logged_services='trafficrouter readiness';
 other_services='dns edge enroller mid origin trafficmonitor trafficops trafficops-perl trafficstats trafficvault';
 docker_compose='docker-compose -f ./docker-compose.yml -f ./docker-compose.readiness.yml';
-time $docker_compose build --parallel $logged_services $other_services;
+time $docker_compose build $logged_services $other_services;
 $docker_compose up -d $logged_services $other_services;
 $docker_compose logs -f $logged_services &
 
