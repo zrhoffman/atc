1091d89b00a46fc6d99d3870fe5127c3cf111c92
Zach Hoffman <zrhoffman@apache.org>
2020-10-19 12:45:38 -0600

    fail TestDeliveryServiceRequestTypeFields

--- traffic_ops/testing/api/v3/deliveryservice_requests_test.go
+++ traffic_ops/testing/api/v3/deliveryservice_requests_test.go
@@ -141,6 +141,7 @@ func TestDeliveryServiceRequestTypeFields(t *testing.T) {
 		if err != nil {
 			t.Errorf("cannot DELETE DeliveryServiceRequest by id: %d - %v - %v", dsrs[0].ID, err, alert)
 		}
+		t.Fatalf("fail TestDeliveryServiceRequestTypeFields")
 	})
 }
 
