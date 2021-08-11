c6163ea33151f02b0766432c9866115d8bd89f5f
Alejandro Bueno <alejandro_bueno@cable.comcast.com>
2021-08-11 09:45:53 -0500

    running docker-entrypoint.sh postgres

--- .github/actions/to-integration-tests/entrypoint.sh
+++ .github/actions/to-integration-tests/entrypoint.sh
@@ -51,16 +51,18 @@ start_traffic_vault() {
 		sed -i '/to-access\.sh\|^to-enroll/d' /etc/riak/{prestart.d,poststart.d}/*
 	BASH_LINES
 
-	DB_PORT=5432
-	DB_SERVER=db
-	DB_NAME=traffic_ops
+	#DB_PORT=5432
+	#DB_SERVER=db
+	#DB_NAME=traffic_ops
 
-	$pg_isready=$(rpm -ql postgresql13 | grep bin/pg_isready);
+	docker-entrypoint.sh postgres &
 
-	while ! $pg_isready -h "$DB_SERVER" -p "$DB_PORT" -d "$DB_NAME"; do
-		echo "waiting for db on $DB_SERVER:$DB_PORT";
-		sleep 3;
-	done
+	#$pg_isready=$(rpm -ql postgresql13 | grep bin/pg_isready);
+
+	#while ! $pg_isready -h "$DB_SERVER" -p "$DB_PORT" -d "$DB_NAME"; do
+	#	echo "waiting for db on $DB_SERVER:$DB_PORT";
+	#	sleep 3;
+	#done
 
 }
 truncate -s0 "${ciab_dir}/traffic.vault.logs";
