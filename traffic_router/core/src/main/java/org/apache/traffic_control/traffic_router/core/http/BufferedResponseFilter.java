apache/trafficcontrol

Add a filter that sets Content-Length header for all responses

traffic_router/core/src/main/java/org/apache/traffic_control/traffic_router/core/http/BufferedResponseFilter.java
@@ -1,0 +1,61 @@
+/*
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.traffic_control.traffic_router.core.http;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.reflect.Field;
+
+public class BufferedResponseFilter extends OncePerRequestFilter {
+	public static final Logger LOGGER = LogManager.getLogger(BufferedResponseFilter.class);
+
+	public void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response, final FilterChain chain) throws IOException, ServletException {
+		final ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
+
+		chain.doFilter(request, responseWrapper);
+
+		if (responseWrapper.getContentSize() == 0) {
+			int contentLength = 0;
+
+			try {
+				final Field contentLengthField = responseWrapper.getClass().getDeclaredField("contentLength");
+				contentLengthField.setAccessible(true);
+				final Object contentLengthObject = contentLengthField.get(responseWrapper);
+				/* E.g., a HEAD request whose corresponding GET request has a non-empty body */
+				if (contentLengthObject != null) {
+					contentLength = (Integer) contentLengthObject;
+				}
+			} catch (ReflectiveOperationException e) {
+				LOGGER.error("Encountered a " + e.getClass() + "exception in " + this.getClass() + ": " + e.getMessage());
+			} finally {
+				response.setContentLength(contentLength);
+			}
+		} else {
+			/* When the content size is greater than 0, copyBodyToResponse()
+			 * will set Content-Length.
+			 */
+			responseWrapper.copyBodyToResponse();
+		}
+	}
+}

zrhoffman/atc

traffic_router/core/src/main/java/org/apache/traffic_control/traffic_router/core/http/BufferedResponseFilter.java
@@ -1,0 +1,60 @@
+/*
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.traffic_control.traffic_router.core.http;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.reflect.Field;
+
+public class BufferedResponseFilter extends OncePerRequestFilter {
+	public static final Logger LOGGER = Logger.getLogger(BufferedResponseFilter.class);
+
+	public void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response, final FilterChain chain) throws IOException, ServletException {
+		final ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
+
+		chain.doFilter(request, responseWrapper);
+
+		if (responseWrapper.getContentSize() == 0) {
+			int contentLength = 0;
+
+			try {
+				final Field contentLengthField = responseWrapper.getClass().getDeclaredField("contentLength");
+				contentLengthField.setAccessible(true);
+				final Object contentLengthObject = contentLengthField.get(responseWrapper);
+				/* E.g., a HEAD request whose corresponding GET request has a non-empty body */
+				if (contentLengthObject != null) {
+					contentLength = (Integer) contentLengthObject;
+				}
+			} catch (ReflectiveOperationException e) {
+				LOGGER.error("Encountered a " + e.getClass() + "exception in " + this.getClass() + ": " + e.getMessage());
+			} finally {
+				response.setContentLength(contentLength);
+			}
+		} else {
+			/* When the content size is greater than 0, copyBodyToResponse()
+			 * will set Content-Length.
+			 */
+			responseWrapper.copyBodyToResponse();
+		}
+	}
+}