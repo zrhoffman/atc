9c2b51c1a04d25f53221a2feb1898dc5bd0eb17c
Zach Hoffman <zrhoffman@apache.org>
2021-12-10 04:12:22 -0700

    Build a Pull Request body from templates

--- .github/actions/assign-triage-role/assign_triage_role/constants.py
+++ .github/actions/assign-triage-role/assign_triage_role/constants.py
@@ -23,4 +23,7 @@ GIT_AUTHOR_EMAIL_TEMPLATE: Final[str] = '{git_author_name}@users.noreply.github.
 TRIAGE_USER_MINIMUM_COMMITS: Final[int] = 2
 SINCE_DAYS_AGO: Final[int] = 31
 ASF_YAML_FILE: Final[str] = '.asf.yaml'
+SINGLE_PR_TEMPLATE_FILE: Final[str] = 'templates/single_pr.md'
+SINGLE_CONTRIBUTOR_TEMPLATE_FILE: Final[str] = 'templates/single_contributor.md'
+PR_TEMPLATE_FILE: Final[str] = 'templates/pr_template.md'
 APACHE_LICENSE_YAML: Final[str] = 'apache_license.yml'
