ddfe69cb9c00a9fe5c68a0d4d63087f44c0fce6b
Zach Hoffman <zrhoffman@apache.org>
2022-03-31 18:13:26 -0600

    continue if ip address is empty

--- .github/actions/tr-ultimate-test-harness/tr-ultimate-test-harness.sh
+++ .github/actions/tr-ultimate-test-harness/tr-ultimate-test-harness.sh
@@ -69,8 +69,10 @@ JSON
 		ip_address="$(<<<"$docker_network" jq -r --arg CONTAINER_ID "$container_id" --arg IP_ADDRESS_FIELD "$ip_address_field" '.[0].Containers[$CONTAINER_ID][$IP_ADDRESS_FIELD]')"
 		if [[ "$ip_address" == null ]]; then
 			echo "Could not find ${ip_address_field} for ${hostname} service!"
+			exit 1
+		fi
+		if [[ "$ip_address" == "" ]]; then
 			continue
-			#exit 1
 		fi
 		interface="$(<<<"$interface" jq --arg IP_ADDRESS "$ip_address" '.ipAddresses += [{} | .address = $IP_ADDRESS | .serviceAddress = true]')"
 	done
