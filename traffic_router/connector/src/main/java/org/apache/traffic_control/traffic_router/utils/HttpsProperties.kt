0b8deffee42c595f82d52257a90c4dd04820c1bd
Zach Hoffman <zrhoffman@apache.org>
2022-10-03 04:41:36 -0600

    Resolve Kotlin errors and warnings

--- traffic_router/connector/src/main/java/org/apache/traffic_control/traffic_router/utils/HttpsProperties.kt
+++ traffic_router/connector/src/main/java/org/apache/traffic_control/traffic_router/utils/HttpsProperties.kt
@@ -20,11 +20,7 @@ import java.nio.file.Paths
 import java.util.function.Consumer
 
 class HttpsProperties {
-    private val httpsPropertiesMap: MutableMap<String?, String?>?
-
-    init {
-        httpsPropertiesMap = HttpsProperties.Companion.loadHttpsProperties()
-    }
+    private val httpsPropertiesMap: MutableMap<String?, String?>? = loadHttpsProperties()
 
     fun getHttpsPropertiesMap(): MutableMap<String?, String?>? {
         return httpsPropertiesMap
@@ -32,11 +28,11 @@ class HttpsProperties {
 
     companion object {
         private val log = LogManager.getLogger(HttpsProperties::class.java)
-        private val HTTPS_PROPERTIES_FILE: String? = "/opt/traffic_router/conf/https.properties"
+        private const val HTTPS_PROPERTIES_FILE: String = "/opt/traffic_router/conf/https.properties"
         private fun loadHttpsProperties(): MutableMap<String?, String?>? {
             return try {
                 val httpsProperties: MutableMap<String?, String?> = HashMap()
-                Files.readAllLines(Paths.get(HttpsProperties.Companion.HTTPS_PROPERTIES_FILE)).forEach(Consumer { propString: String? ->
+                Files.readAllLines(Paths.get(HTTPS_PROPERTIES_FILE)).forEach(Consumer { propString: String ->
                     if (!propString.startsWith("#")) { // Ignores comments in properties file
                         val prop: Array<String?> = propString.split("=".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
                         httpsProperties[prop[0]] = prop[1]
@@ -44,7 +40,7 @@ class HttpsProperties {
                 })
                 httpsProperties
             } catch (e: Exception) {
-                HttpsProperties.Companion.log.error("Error loading https properties file.")
+                log.error("Error loading https properties file.")
                 null
             }
         }
